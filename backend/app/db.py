810407059c555c8390d24cb9d32e04c1f3afba40
Mercedes Slepowron <mslepowron@fi.uba.ar>
2025-10-19 19:09:38 -0300

    agrego persistencia de la bdd frente a la baja del contenedor del docker

--- backend/app/db.py
+++ backend/app/db.py
@@ -4,6 +4,16 @@ import os
 
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./plan_go.db")
+
+if DATABASE_URL.startswith("sqlite"):
+    # Extrae la ruta del archivo de la URL
+    db_file_path = DATABASE_URL.split("///")[1]
+    # Extrae el directorio del archivo
+    db_directory = os.path.dirname(db_file_path)
+    # Crea el directorio si no existe
+    if db_directory:
+        os.makedirs(db_directory, exist_ok=True)
+        
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
